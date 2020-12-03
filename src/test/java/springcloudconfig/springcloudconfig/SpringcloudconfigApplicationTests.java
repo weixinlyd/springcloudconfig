75a641d230499029f0636bb59b856023b465c6b7
lyd <yanda@shenzhourong.com>
2020-12-03 10:17:33 +0800

    Initial commit

---
+++ src/test/java/springcloudconfig/springcloudconfig/SpringcloudconfigApplicationTests.java
@@ -0,0 +1,13 @@
+package springcloudconfig.springcloudconfig;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcloudconfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
